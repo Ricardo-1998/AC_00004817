448ee5d0cbcf12c8a1ac43f89fd41e057f341fa2
Ricardo-1998 <00004817@uca.edu.sv>
2020-04-25 17:05:13 -0600

    segundo ejercicio terminado

--- tarea_2_00004817.asm
+++ tarea_2_00004817.asm
@@ -1,42 +1,66 @@
-org	100h
-
-mov	ax, 0000h
-mov	bx, 0000h
-mov	ax, 4d
-mov	bx, 8d
-add 	ax,bx
-mov	bx, 0000h
-mov	bx, 1d
-add 	ax,bx
-mov	bx, 0000h
-mov	bx, 7d
-add 	ax,bx
-mov	bx, 0000h
-mov 	bx, 4d
-div 	bx
+	org	100h
+;Inicio ejercicio 1----------------------------------------------------------
+	mov	ax, 0000h
+	mov	bx, 0000h
+	mov	ax, 4d
+	mov	bx, 8d
+	add 	ax,bx
+	mov	bx, 0000h
+	mov	bx, 1d
+	add 	ax,bx
+	mov	bx, 0000h
+	mov	bx, 7d
+	add 	ax,bx
+	mov	bx, 0000h
+	mov 	bx, 4d
+	div 	bx
 ;En ax queda guardado que es 5 el promedio frase "en el segundo"
-mov 	cl, "E"
-mov 	[200h], cl
-mov 	cl, "N"
-mov 	[201h], cl
-mov 	cl, "E"
-mov 	[203h], cl
-mov 	cl, "L"
-mov 	[204h], cl
-mov 	cl, "S"
-mov 	[206h], cl
-mov 	cl, "E"
-mov 	[207h], cl
-mov 	cl, "G"
-mov 	[208h], cl
-mov 	cl, "U"
-mov 	[209h], cl
-mov 	cl, "N"
-mov 	[20Ah], cl
-mov 	cl, "D"
-mov 	[20Bh], cl
-mov 	cl, "O"
-mov 	[20Ch], cl
+	mov 	cl, "E"
+	mov 	[200h], cl
+	mov 	cl, "N"
+	mov 	[201h], cl
+	mov 	cl, "E"
+	mov 	[203h], cl
+	mov 	cl, "L"
+	mov 	[204h], cl
+	mov 	cl, "S"
+	mov 	[206h], cl
+	mov 	cl, "E"
+	mov 	[207h], cl
+	mov 	cl, "G"
+	mov 	[208h], cl
+	mov 	cl, "U"
+	mov 	[209h], cl
+	mov 	cl, "N"
+	mov 	[20Ah], cl
+	mov 	cl, "D"
+	mov 	[20Bh], cl
+	mov 	cl, "O"
+	mov 	[20Ch], cl
+;Fin ejercicio 1----------------------------------------------------------
+
+;Inicio ejericio 2--------------------------------------------------------
+	mov	ax, 0000h
+	mov 	ax, 4d
+	mov	bx, 0000h
+	mov	bx, 2d
+	mov 	si, 0d
 
 
-int	20h;
+
+lupita:	mov [si+210h],ax
+	mul	bx
+	inc	si
+	cmp	ax,100h
+	jb	lupita
+
+lupita2:mov [si+210h],ax
+	mul	bx
+	inc	si
+	inc 	si
+	cmp 	si,16d
+	jne	lupita2
+
+	
+;Fin ejericio 2--------------------------------------------------------
+
